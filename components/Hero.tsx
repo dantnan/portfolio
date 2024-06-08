d7b4a377e9f528a1be76edfb1389fc1a0147fc0f
Antnan <dantnan@gmail.com>
2024-06-09 02:35:24 +0300

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className='pb-20 pt-36'>
             <div>
                 <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill="white" />
-                <Spotlight className='top-10 -left-full h-[80vh] w-[50vw]' fill="purple" />
+                <Spotlight className='top-10 -left-full h-[80vh] w-[50vw]' fill="purple"/>
                 <Spotlight className='top-28 -left-100 h-[80vh] w-[50vw]' fill="blue" />
             </div>
             <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
