4e40b9c5421f70058b06cbadd046b12e930c5d25
Antnan <dantnan@gmail.com>
2024-06-10 02:24:55 +0300

    Recent Projects finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
 
 
@@ -8,11 +11,11 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          { name: "Home", link: "/", icon:<FaHome /> }
-          ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid /> 
+        <Grid />
+        <RecentProjects /> 
+        <Clients />
       </div>
     </main>
   );
---
+++ components/Clients.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Clients = () => {
+    return (
+        <div className='py-20' id='projects'>
+            <h1 className='heading'>Kind words from
+                <span className='text-purple'> satisfied clients</span>
+            </h1>
+            <div className='flex flex-col items-center'>
+            </div>
+        </div>
+    )
+}
+
+export default Clients
