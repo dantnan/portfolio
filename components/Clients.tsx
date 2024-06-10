f50e35a47bb2e430222d40f95fa3b0df6b9d273b
Antnan <dantnan@gmail.com>
2024-06-10 18:26:44 +0300

    complete

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
+import { companies, testimonials } from '@/data'
 
 const Clients = () => {
     return (
-        <div className='py-20' id='projects'>
+        <div className='py-20' id='testimonials'>
             <h1 className='heading'>Kind words from
                 <span className='text-purple'> satisfied clients</span>
             </h1>
-            <div className='flex flex-col items-center'>
+            <div className='flex flex-col items-center max-lg:mt-10'>
+                <InfiniteMovingCards
+                    items={testimonials}
+                    direction='left'
+                    speed='slow'
+                />
+                <div className='flex flex-wrap items-center justify-center md:gap-16 gap-4 mt-10'>
+
+                    {companies.map(({ id, img, name, nameImg }) => (
+                        <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
+                            <img src={img} alt={name} className='md:w-210 w-5' />
+                            <img src={nameImg} alt={name} className='md:w-24 w-20' />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
---
+++ components/Footer.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data'
+import { WiDayThunderstorm } from 'react-icons/wi'
+
+const Footer = () => {
+    return (
+        <footer className='w-full pb-10 mb-[100px] md:mb-5' id='contact'>
+
+            <div className='flex flex-col items-center'>
+
+                <h1 className='heading lg:max-w-[45vw]'>Ready to take <span className='text-purple'>your</span> digital presence to the next level</h1>
+                <p className='text-white-100 md:mt-10 my-5 text-center'>
+                    Reach out to me today and let&apos;s discuss how I can help you achieve your goals.
+                </p>
+                <a href="mailto:2t9Zn@example.com" className='btn btn-primary'>
+                    <MagicButton
+                        title='Let&apos;s get in touch'
+                        icon={<FaLocationArrow />}
+                        position={'right'}
+                    />
+                </a>
+            </div>
+            <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+                <p className='md:text-base text-sm md:font-normal font-light'>
+                    Copyright &copy; {new Date().getFullYear()} antnan.com
+                </p>
+                <div className='flex items-center md:gap-3 gap-6'>
+                    {socialMedia.map((profile) => (
+                        <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-20 rounded-lg border border-black-300'>
+                            <img src={profile.img} 
+                            alt='icons'
+                            width={20}
+                            height={20}
+                            />
+                        </div>
+
+                    ))}
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
